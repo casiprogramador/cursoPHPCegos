bcb3715b29117331689b263a3bd173a64e590792
Marcelo Choque <marcelo.choque@oep.org.bo>
2019-01-19 09:18:41 -0400

    actualizacion clase 11

--- clase11/contacto.php
+++ clase11/contacto.php
@@ -5,31 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <title>Contacto</title>
+    <title>Contactos</title>
 </head>
 <body>
-    <h1>Formulario Contacto</h1>
+    <h1>Formulario Contactos</h1>
     <div class="formulario">
-    <form action="enviado.php" method="post" id="formulario-contacto">
-        <label for="nombre">Nombre:</label>
-        <br>
-        <input type="text" name="nombre" id="id-nombre">
-        <br>
-        <label for="email">Correo:</label>
-        <br>
-        <input type="text" name="email" id="id-email">
-        <br>
-        <label for="mensaje">Mensaje:</label>
-        <br>
-        <textarea name="mensaje" cols="30" rows="10" name="mensaje" id="id-mensaje"></textarea>
-        <br>
-        <input type="submit" value="Enviar Mensaje">
-    </form>
+        <form action="enviado.php" method="post" id="formulario-contacto">
+            <label for="nombre">Nombre:</label>
+            <br>
+            <input type="text" name="nombre" id="id-nombre">
+            <span class="error error-nombre">Debe ingresar el nombre</span>
+            <br>
+            <label for="email">Email:</label>
+            <br>
+            <input type="text" name="email" id="id-email">
+            <span class="error error-email">Debe ingresar el correo</span>
+            <br>
+            <label for="mensaje">Mensaje:</label>
+            <br>
+            <textarea name="mensaje" cols="30" rows="10" id="id-mensaje"></textarea>
+            <span class="error error-mensaje">Debe ingresar su mensaje</span>
+            <br>
+            <input type="submit" value="Enviar">
+        </form>
     </div>
-    <script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
+    <script src="jquery.js"></script>
     <script src="script.js"></script>
 </body>
 </html>
--- clase11/script.js
+++ clase11/script.js
@@ -1,36 +1,42 @@
-$(document).ready(function() {
- 
-    $('#formulario-contacto').submit(function(e) {
-      
-      var id_nombre = $('#id-nombre').val();
-      var id_email = $('#id-email').val();
-      var id_mensaje = $('#id-mensaje').val();
-   
-      //$(".error").remove();
+$(document).ready(function(){
+  console.log('Jquery RUN');
+  $(".error-nombre").hide();
+  $(".error-email").hide();
+  $(".error-mensaje").hide();
 
-      var validado = true;
-   
-      if (id_nombre.length < 1) {
-        $('#id-nombre').after('<span class="error">El nombre es requerido</span>');
-        validado = false;
-      }
+  $('#formulario-contacto').submit(function(e){
 
-      if (id_email.length < 1) {
-        $('#id-email').after('<span class="error">El correo es requerido</span>');
-        validado = false;
-      }
+    var validado = true;
+    // Nombre
+    var id_nombre = $('#id-nombre').val();
+    console.log(id_nombre);
 
-      if (id_mensaje.length < 1) {
-        $('#id-mensaje').after('<span class="error">El mensaje es requerido</span>');
-        validado = false;
-      }
-      // Si el formulario es valido enviara el mensaje
-      if(validado){
-        return;
-      }else{
-        e.preventDefault();
-      }
+    if(id_nombre.length < 1){
+      $(".error-nombre").show();
+      validado = false;
+    }
 
-    });
-   
-  });
+    // Email
+    var id_email = $('#id-email').val();
+    console.log(id_email);
+    if(id_email.length < 1){
+      $(".error-email").show();
+      validado = false;
+    }
+
+    // Mensaje
+    var id_mensaje = $('#id-mensaje').val();
+    console.log(id_mensaje);
+    if(id_mensaje.length < 1){
+      $(".error-mensaje").show();
+      validado = false;
+    }
+
+    if(validado){
+      return;
+    }else{
+      e.preventDefault();
+    }
+
+  })
+});
